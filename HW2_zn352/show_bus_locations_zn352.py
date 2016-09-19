d4d0bcfeeefb2e5bf0166840fb532c9a24596a3a
EstherNiu <estherzhn@gmail.com>
2016-09-19 10:31:21 -0400

    Update show_bus_locations_zn352.py

--- HW2_zn352/show_bus_locations_zn352.py
+++ HW2_zn352/show_bus_locations_zn352.py
@@ -6,8 +6,7 @@ import sys
 
 key = sys.argv[1]   # key = '056a5fec-16fc-4372-82a6-358369262821'
 busline = sys.argv[2]   # busline = 'B52'
-url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?\
-key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s"%(key,busline)
+url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s"%(key,busline)
 response = urllib2.urlopen(url)
 data = response.read().decode("utf-8")
 jsondata = json.loads(data)
